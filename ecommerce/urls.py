1d5462e58ed2488f3e78ebda221dd516eda99da3
V@runDby <135414171+varundubey16@users.noreply.github.com>
2026-06-29 21:50:49 +0530

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,11 +1,3 @@
-"""
-
-Developed By : sumit kumar
-facebook : fb.com/sumit.luv
-Youtube :youtube.com/lazycoders
-
-
-"""
 from django.contrib import admin
 from django.urls import path
 from ecom import views
